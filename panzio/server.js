119c48ab4e22d2e43770d0bafdbb7e398689e34e
gygabika <82406669+gygabika@users.noreply.github.com>
2025-03-03 13:26:09 +0100

    lekérdezés
    szobaId foglaltsag

--- panzio/server.js
+++ panzio/server.js
@@ -6,6 +6,7 @@ const nodemon = require("nodemon");
 const mysql = require("mysql");
 app.use(bodyParser.json());
 app.use(cors());
+const PORT = process.env.PORT || 3000;
 
 const db = mysql.createConnection({
     user:"root",
@@ -16,41 +17,55 @@ const db = mysql.createConnection({
 })
 
 app.get("/", (req,res) => {
-    res.send("Fut a backend!")
+    res.send("Fut a backend!");
 });
 
 app.listen(3567, () => {
-    console.log("A szerveren a 3567 porton fut!")
+    console.log("A szerveren a 3567 porton fut!");
 })
 
 app.get("/",( req, res) => {
     const sql ="SELECT * FROM ``";
     db.query(sql, (err,result) => {
-        if (err) return res.json(err);
-        return res.json(result)
+        if (err) return res.status(500).json({error: err.message});
+        res.json(result);
     })
 })
  
 app.get("/szobak",( req, res) => {
     const sql = "SELECT * FROM `szobak`";
     db.query(sql, (err,result) => {
-        if (err) return res.json(err);
-        return res.json(result)
+        if (err) return res.status(500).json({error: err.message});
+        res.json(result);
     })
 })
 
 app.get("/foglalasok",( req, res) => {
     const sql = "SELECT szobak.szazon AS szoba, COUNT(DISTINCT foglalasok.vendeg) AS vendegek, SUM(DATEDIFF(foglalasok.tav, foglalasok.erk)) AS vendegejszakak FROM szobak JOIN foglalasok ON szobak.szazon = foglalasok.szoba GROUP BY szobak.szazon ORDER BY vendegejszakak ASC, vendegek ASC;";
     db.query(sql, (err,result) => {
-        if (err) return res.json(err);
-        return res.json(result)
+        if (err) return res.status(500).json({error: err.message});
+        res.json(result);
     })
 })
 
 app.get("/foglaltsag",( req, res) => {
-    const sql = "SELECT vendegek.vnev AS nev, foglalasok.erk AS erkezes, foglalasok.tav AS tavozo FROM foglalasok JOIN vendegek ON foglalasok.vendeg = vendegek.vsorsz WHERE foglalasok.szoba IN (1, 2, 3) ORDER BY vendegek.vnev ASC;";
+    const sql = "SELECT vendegek.vnev AS nev, foglalasok.erk AS erkezes, foglalasok.tav AS tavozas FROM foglalasok JOIN vendegek ON foglalasok.vendeg = vendegek.vsorsz WHERE foglalasok.szoba IN (1, 2, 3) ORDER BY vendegek.vnev ASC;";
     db.query(sql, (err,result) => {
-        if (err) return res.json(err);
-        return res.json(result)
+        if (err) return res.status(500).json({error: err.message});
+        res.json(result);
     })
+})
+
+app.get("/foglaltsag/:szobaId",( req, res) => {
+    const szobaId = req.params.szobaId;
+    db.query("SELECT vendegek.vnev AS nev, foglalasok.erk AS erkezes, foglalasok.tav AS tavozas FROM foglalasok JOIN vendegek ON foglalasok.vendeg = vendegek.vsorsz WHERE foglalasok. szoba = ? ORDER BY vendegek.vnev ASC;",
+    [szobaId],
+    (err,result) => {
+        if (err) return res.status(500).json({error: err.message});
+        res.json(result);
+    })
+})
+
+app.listen(PORT, () => {
+    console.log("Server fut:");
 })
